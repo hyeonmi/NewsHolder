dde745de75c15c29f5d82e929d37af3a81bb6208
hyeon mi <pilotfish22@gmail.com>
2012-08-23 13:48:07 +0900

    localstore id type -> sequential

--- app/model/MainModel.js
+++ app/model/MainModel.js
@@ -2,6 +2,8 @@ Ext.define('NewsHolder.model.MainModel', {
     extend: 'Ext.data.Model',
 
     config: {
+    	idProperty: 'id',
+    	identifier : 'sequential',
         fields: [
                  {
                 	 name : 'id', type: 'auto'
