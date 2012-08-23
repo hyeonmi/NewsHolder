9d5f89acf0b01d43208b286c72070ddc440a11a0
bcf112 <bcf112@naver.com>
2012-08-23 15:58:20 +0900

    extractor upgrade

--- app/util/TagExtractor.js
+++ app/util/TagExtractor.js
@@ -1,51 +1,27 @@
 Ext.define('NewsHolder.util.TagExtractor', {
 	constructor : function(config) {
-		console.log("initialize()");
 		existTag = false;
-		tagCount = 0;
+		tagNumber = 0;
 	},
 
-	extractTag : function(tag, store, controller, record) {
+	extractTag : function(tag, store, controller, record) {	
 		if (tag == null) {
 			console.log("태그를 지정해주세요.");
 			return;
 		}
 
-		console.log(tagCount);
-
-		// for(var i=0; i<store.getData().length; i++){
-		// //console.log(store.getData().items[i].data.description);
-		// if(store.getData().items[i].data.description.match("img")){
-		// flag = true;
-		// count = i;
-		// }
-		// }
-
+		//파라미터 값 tag가 description에 들어있는지를 검색
+		//태그가 있으면 existTag를 true로 설정하고, 이미지가 있는 마지막 기사의 index를 tagNumber에 할당.
 		for ( var i = 0; i < store.getData().length; i++) {
-			// console.log(store.getData().items[i].data.description);
 			if (store.getData().items[i].data.description.match(tag)) {
 				existTag = true;
-				tagCount++;
+				tagNumber = i;
 			}
 		}
-
-		/*
-		 * if(existTag=="img"){ var data = { url :
-		 * store.getData().items[count].data.description.split('img
-		 * src="')[1].split('"')[0], title :
-		 * store.getData().items[count].data.title, };
-		 * this.getNewsListTopImage().setData(data); }else{ console.log("이미지가
-		 * 포함된 기사가 없으빈다..."); data = { url : record.data.image_url, title :
-		 * store.getData().items[count].data.title, };
-		 * this.getNewsListTopImage().setData(data); }
-		 */
-
-		
 		
 		if (tag == "img" && existTag) {
-			
-			var news = store.getData().items[tagCount].data;
-			
+
+			var news = store.getData().items[tagNumber].data;
 			var data = {
 				url : news.description.split('img src="')[1].split('"')[0],
 				title : news.title,
@@ -55,10 +31,34 @@ Ext.define('NewsHolder.util.TagExtractor', {
 			console.log("이미지가 포함된 기사가 없으빈다...");
 			data = {
 				url : record.data.image_url,
-				title : store.getData().items[tagCount].data.title,
+				title : store.getData().items[tagNumber].data.title,
 			};
 			controller.getNewsListTopImage().setData(data);
 		}
+	},
+	
+	//모든 a태그를 삭제하는 부분.
+	removeATag:function(store){
+		var aTagRE = "/<[aA][^>]*>.*</[aA]>/";
+		//var removeTagContent=content.replace(aTagRE,"");
+		
+		for(var i=0; i<store.getData().items.length; i++){
+			store.getData().items[i].data.description=store.getData().items[i].data.description.replace(aTagRE,"");
+		}
+		//return removeTagContent;
 	}
+	
+	
+/*	//모든 a태그를 삭제하는 부분.
+	removeATag:function(content){
+		var aTagRE = "/<[aA][^>]*>.*</[aA]>/";
+		var removeTagContent=content.replace(aTagRE,"");
+		
+		for(var i=0; i<store.length; i++){
+			var mDesc=extractor.removeATag(store.getData().items[i].description);
+				this.getList().setData()
+			}
+		//return removeTagContent;
+	}*/
 
 });
