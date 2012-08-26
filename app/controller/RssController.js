f87177b3fcf36b645ee491ffe2ae708546a11f04
hyeon mi <pilotfish22@gmail.com>
2012-08-27 00:31:01 +0900

    refactoring

--- app/controller/RssController.js
+++ app/controller/RssController.js
@@ -50,9 +50,9 @@ Ext.define('NewsHolder.controller.RssController', {
 
 	// rss add
 	onRssAddButtonTap : function(button, e, options) {
-		var rssname = Ext.getCmp('rssNameText').getValue();
+		//var rssname = Ext.getCmp('rssNameText').getValue();
 		var rssurl = Ext.getCmp('rssUrlText').getValue();
-		var rssimg = Ext.getCmp('rssImageText').getValue();
+		var rssimg = './resources/images/rss_tmp.png';
 		if (rssurl == "") {
 			Ext.Msg.alert('확인', 'URL을 입력해주세요.', Ext.emptyFn);
 			return false;
@@ -66,29 +66,19 @@ Ext.define('NewsHolder.controller.RssController', {
 			callback : function() {
 				var store = Ext.getStore('newsPaperStore');
 				var title = store.data.items[0].get('title');
+
 				if(title == ""){
 					Ext.Msg.alert("확인","존재하지 않습니다.");
-					return;
 				}else{
 					Ext.Msg.confirm("확인", title + "이 맞습니까?", function(buttonId, value, opt){
 						if(buttonId =='yes'){
-							var store = Ext.getStore('mainStore');
-							// 중복확인
-							if (store.find('mainRssUrl', rssurl) > -1) {
-								Ext.Msg.alert('확인', '이미 저장되어 있습니다.');
-							} else {
-								store.add({
-									mainRssName : rssname,
-									mainRssUrl : rssurl,
-									mainRssImage : rssimg
-								});
-								store.sync();
-							}
+							this.addMainLocalStore(title, rssurl, rssimg);
 						}
 						
 					}, this);
 				}
-			}
+			},
+			scope : this
 		});
 		// 유효한 rss인지 확인
 
@@ -101,29 +91,28 @@ Ext.define('NewsHolder.controller.RssController', {
 		var rssurl = record.get('rssUrl');
 		var rssimg = record.get('rssImage');
 
-		this.addMainLocalStore(rssname, rssurl, rssimg);
+		Ext.Msg.confirm('확인', '추가하시겠습니까?', function(buttonId, value, opt) {
+			if (buttonId == 'yes') {
+				this.addMainLocalStore(rssname, rssurl, rssimg);
+			}
+		}, this);
 
 	},
 
 	// function add item MainLocalStored
 	addMainLocalStore : function(rssname, rssurl, rssimg) {
-		Ext.Msg.confirm('확인', '추가하시겠습니까?', function(buttonId, value, opt) {
-			if (buttonId == 'yes') {
-				var store = Ext.getStore('mainStore');
-				// 중복확인
-				if (store.find('mainRssUrl', rssurl) > -1) {
-					Ext.Msg.alert('확인', '이미 저장되어 있습니다.');
-				} else {
-					store.add({
-						mainRssName : rssname,
-						mainRssUrl : rssurl,
-						mainRssImage : rssimg
-					});
-					store.sync();
-				}
+		var store = Ext.getStore('mainStore');
 
-			}
-		}, this);
+		if (store.find('mainRssUrl', rssurl) > -1) { //중복확인
+			Ext.Msg.alert('확인', '이미 저장되어 있습니다.');
+		} else {
+			store.add({
+				mainRssName : rssname,
+				mainRssUrl : rssurl,
+				mainRssImage : rssimg
+			});
+			store.sync();
+		}
 
 	},
 
