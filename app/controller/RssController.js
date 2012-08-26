cd09ed96d20aff26a476f80c901ed114907302a8
hyeon mi <pilotfish22@gmail.com>
2012-08-26 18:31:01 +0900

    add validation

--- app/controller/RssController.js
+++ app/controller/RssController.js
@@ -1,86 +1,94 @@
 Ext.define('NewsHolder.controller.RssController', {
-    extend: 'Ext.app.Controller',
+	extend : 'Ext.app.Controller',
 
-    config: {
-        models: [
-            'RssModel'
-        ],
-        views: [
-            'RssPanel'
-        ],
+	config : {
+		models : [ 'RssModel' ],
+		views : [ 'RssPanel' ],
 
-        refs: {
-            addButton: '#rssAddButton',
-            rssItems: '#rssList',
-            	
-        },
-        
-        control: {
-            'addButton': {
-                tap: 'onRssAddButtonTap'
-            },
-            'rssItems': {
-                itemtap: 'onRssListItemTap'
-            }
-        },
-  
-    },
-    
-    // sever->local
-    init : function(){
-    	var serverstore = Ext.getStore('rssServerStore');    	
-    	serverstore.load(
-    				{
-    					callback : function(){
-    						success : {
-    						var localstore = Ext.getStore('rssStore');
-	    				    	if(localstore.data.length <= 0){
-	    				    		for(var i=0; i < serverstore.data.length; i++){
+		refs : {
+			addButton : '#rssAddButton',
+			rssItems : '#rssList',
 
-	    				    			localstore.add({ rssName : serverstore.data.items[i].get('rssName'),
-	    				    	        	rssUrl : serverstore.data.items[i].get('rssUrl') ,
-	    				    	        	rssImage : serverstore.data.items[i].get('rssImage') });
-	    				    	             			
-	    				    		}
-	    				    		localstore.sync();
-	    				    		
-	    				    	}    
-    						}
-    					
-    					}
-    				}
-    		); 
-    	
-    },
-    
-    
-    // rss add
-    onRssAddButtonTap: function(button, e, options) {
-        var rssname = JSON.stringify(Ext.getCmp('rssNameText').getValue());
-        var rssurl = Ext.JSON.encode(Ext.getCmp('rssUrlText').getValue());
-        var rssimg = Ext.JSON.encode(Ext.getCmp('rssImageText').getValue());
-        
-        this.addMainLocalStore(rssname, rssurl, rssimg);
-    },
+		},
 
-    // news rss add
-    onRssListItemTap: function(dataview, index, target, record, e, options) {
-        var rssname = record.get('rssName');
-        var rssurl = record.get('rssUrl');
-        var rssimg = record.get('rssImage');
+		control : {
+			'addButton' : {
+				tap : 'onRssAddButtonTap'
+			},
+			'rssItems' : {
+				itemtap : 'onRssListItemTap'
+			}
+		},
 
-        this.addMainLocalStore(rssname, rssurl, rssimg);
-            	
-    },
-    
-    // function add item MainLocalStored
-    addMainLocalStore : function(rssname, rssurl, rssimg){
-    	var store = Ext.data.StoreManager.lookup('mainStore');
-        store.add({ mainRssName : rssname,
-        	mainRssUrl : rssurl ,
-        	mainRssImage : rssimg });
-        store.sync();       	
-    }         
-    
+	},
+
+	// sever->local
+	init : function() {
+		var serverstore = Ext.getStore('rssServerStore');
+		serverstore.load({
+			callback : function() {
+				success: {
+					var localstore = Ext.getStore('rssStore');
+					if (localstore.data.length <= 0) {
+						for ( var i = 0; i < serverstore.data.length; i++) {
+
+							localstore.add({
+								rssName : serverstore.data.items[i]
+										.get('rssName'),
+								rssUrl : serverstore.data.items[i]
+										.get('rssUrl'),
+								rssImage : serverstore.data.items[i]
+										.get('rssImage')
+							});
+
+						}
+						localstore.sync();
+
+					}
+				}
+
+			}
+		});
+
+	},
+
+	// rss add
+	onRssAddButtonTap : function(button, e, options) {
+		var rssname = Ext.getCmp('rssNameText').getValue();
+		var rssurl = Ext.getCmp('rssUrlText').getValue();
+		var rssimg = Ext.getCmp('rssImageText').getValue();
+		if (rssurl == "") {
+			Ext.Msg.alert('확인', 'URL을 입력해주세요.', Ext.emptyFn);
+		}
+
+		this.addMainLocalStore(rssname, rssurl, rssimg);
+	},
+
+	// news rss add
+	onRssListItemTap : function(dataview, index, target, record, e, options) {
+		var rssname = record.get('rssName');
+		var rssurl = record.get('rssUrl');
+		var rssimg = record.get('rssImage');
+
+		Ext.Msg.confirm('확인', '신문사를 추가하시겠습니까?', 
+				function(buttonId, value, opt) {
+			if (buttonId == 'yes') {
+				this.addMainLocalStore(rssname, rssurl, rssimg);
+
+			}
+		}, this);
+
+	},
+
+	// function add item MainLocalStored
+	addMainLocalStore : function(rssname, rssurl, rssimg) {
+		var store = Ext.data.StoreManager.lookup('mainStore');
+		store.add({
+			mainRssName : rssname,
+			mainRssUrl : rssurl,
+			mainRssImage : rssimg
+		});
+		store.sync();
+	},
 
 });
