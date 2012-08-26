13d2752b471a97edd014759f38495a6093d25bc9
iamapark <iamapark89@daum.net>
2012-08-26 14:41:24 +0900

    Article.js 수정

--- app/view/Article.js
+++ app/view/Article.js
@@ -55,27 +55,36 @@ Ext.define("NewsHolder.view.Article",{
 		listeners:{
 			swipe:{
 				fn:function(event){
+					var navi = localStorage.History_navigator;
+					var data = null;
+					
 					var flag = localStorage.flag;  //현재 보고 있는 기사가 전체 기사 중 몇번째인지 알려주는 변수
 					var count = Ext.getStore("Feed").data.length;  //전체 기사가 총 몇 개인지 알려주는 변수
 					var panel = Ext.getCmp("articlePanel");        //기사 전문 패널을 가져옵니다.
 					var titlebar = Ext.getCmp("titlebar");         //기사 타이틀바를 가져옵니다.
 					
+					if(navi=="News"){
+						data = Ext.getStore("Feed").data;
+					}else if(navi=="Search"){
+						data = Ext.getStore("searchResultStore").data;
+					}
+					
+					console.log(data);
+					
 					if(event.direction == "left"){ //다음 기사로 이동
 						if(flag==count){
 							Ext.Msg.alert("알림", "다음 기사가 없습니다.");
 						}else{
-							panel.setData(Ext.getStore("Feed").data.items[++flag].data);
-							titlebar.setTitle(Ext.getStore("Feed").data.items[flag].data.title);
+							panel.setData(data.items[++flag].data);
+							titlebar.setTitle(data.items[flag].data.title);
 							localStorage.flag = flag;
 						}
 					}else if(event.direction = "right"){  //이전 기사로 이동
-						console.log("이전 기사");
-						
 						if(flag==0){
 							Ext.Msg.alert("알림", "이전 기사가 없습니다.");
 						}else{
-							panel.setData(Ext.getStore("Feed").data.items[--flag].data);
-							titlebar.setTitle(Ext.getStore("Feed").data.items[flag].data.title);
+							panel.setData(data.items[--flag].data);
+							titlebar.setTitle(data.items[flag].data.title);
 							localStorage.flag = flag;
 						}
 					}
