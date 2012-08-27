7d4be56bf57ac74e9b11adcb822724db7d8f366b
hyeon mi <pilotfish22@gmail.com>
2012-08-27 15:51:33 +0900

    ScrapList modify

--- app/controller/ScrapController.js
+++ app/controller/ScrapController.js
@@ -32,24 +32,21 @@ Ext.define('NewsHolder.controller.ScrapController', {
     },
     /** 스크랩 리스트에서 기사 제목을 탭했을 때 실행 */// /////////////////////////////////////
     scrapListTap:function(list, index, item, record, e){
-    	console.log(list.lastTapHold);
-    	if (!list.lastTapHold || (list.lastTapHold - new Date() > 1000)) {
-            console.log('itemtap');
+    	if (!list.lastTapHold || (new Date() - list.lastTapHold  > 1000)) {
+            console.log('ahahah');
+            var Maincontroller = this.getApplication().getController("MainController");
+        	Maincontroller.getMain().animateActiveItem(
+        			this.getArticle(), {
+        				type:"slide",
+        				direction : "left"
+        			});
+        	Maincontroller.getArticleList().setData(record.data);
+        	this.getScrapBackButton().show();
+        	this.getScrapHomeButton().hide();
+        	
+        	localStorage.flag = index;
+        	Ext.getCmp("articleScrapButton").show();
         }
-    	
-    	console.log("list item tap");
-    	var Maincontroller = this.getApplication().getController("MainController");
-    	Maincontroller.getMain().animateActiveItem(
-    			this.getArticle(), {
-    				type:"slide",
-    				direction : "left"
-    			});
-    	Maincontroller.getArticleList().setData(record.data);
-    	this.getScrapBackButton().show();
-    	this.getScrapHomeButton().hide();
-    	
-    	localStorage.flag = index;
-    	Ext.getCmp("articleScrapButton").show();
     },
     
     scrapBackButtonTap:function(button, e, options){
@@ -72,25 +69,14 @@ Ext.define('NewsHolder.controller.ScrapController', {
     },
     
     scrapListTapHold:function(list, item, index, e, eOpts){
+    	this.getScrapList().deselectAll();
     	list.lastTapHold = new Date();
-    	console.log(list.lastTapHold);
-        console.log('itemtaphold');
     	Ext.Msg.confirm("알림", "해당 스크랩을 삭제하시겠습니까", function(buttonId, value, opt){
     		if(buttonId=="yes"){
-    			console.log(record.internalId);
         		var store = Ext.getStore('Scraps');
         		store.remove(record);
         		store.sync();
     		}
     	}, this);
     },
-    
-    onItemDisclosure : function(record,btn,index){
-        alert(record.get('hour') + ',' + record.get('wfKor'));
-          }
-
-
-
-    
-    
 });
