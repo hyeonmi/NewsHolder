71128fdc83d14b1aeb7705098d09573f900594a9
bcf112 <bcf112@naver.com>
2012-08-31 00:48:12 +0900

    ManagerController Panel 넘기는 부분 조금 더 업데이트
    예외처리 좀 했습니다. 그리고 ButtonController 다 고쳤습니다.

--- app/util/ManagerController.js
+++ app/util/ManagerController.js
@@ -1,48 +1,66 @@
-Ext.define('NewsHolder.util.ManagerController',{
+Ext.define('NewsHolder.util.ManagerController', {
 	xtype : 'managerController',
 	id : 'managerControllerId',
-	
-	constructor : function(config){
+
+	constructor : function(config) {
 		this.initConfig(config);
 	},
-	
+
 	config : {
-		
+
 	},
-	
-	//왼쪽으로 이동하는 에니메이션
-	//param : title(지정하고자 하는 타이틀바 제목), movePanel(이동하고자 하는 패널 객체)
-	//hideComp(숨기고자 하는 컴포넌트 객체배열), showComp(보여주고자 하는 컴포넌트 id 배열)
-	onMoveSlideLeft : function(title, movePanelId, hideCompId, showCompId){
+
+	// 왼쪽으로 이동하는 에니메이션
+	// param : title(지정하고자 하는 타이틀바 제목), movePanel(이동하고자 하는 패널 객체)
+	// hideComp(숨기고자 하는 컴포넌트 객체배열), showComp(보여주고자 하는 컴포넌트 id 배열)
+	onMoveSlideLeft : function(title, movePanelId, hideCompId, showCompId) {
 		Ext.getCmp('titlebar').setTitle(title);
-		Ext.getCmp('mainPanel').animateActiveItem(Ext.getCmp(movePanelId), {
-			type : 'slide',
-			direction : 'left'
-		});
 		
-		for(var i=0; i<hideCompId.length; i++){
-			Ext.getCmp(hideCompId[i]).hide();
+		if(typeof(movePanelId)=='string'){
+			Ext.getCmp('mainPanel').animateActiveItem(Ext.getCmp(movePanelId), {
+				type : 'slide',
+				direction : 'left'
+			});
+		}else if(typeof(movePanelId)=='number'){
+			Ext.getCmp('mainPanel').animateActiveItem(movePanelId, {
+				type : 'slide',
+				direction : 'left'
+			});
 		}
 		
-		for(var j=0; j<showCompId.length; j++){
+
+		for ( var i = 0; i < hideCompId.length; i++) {
+			Ext.getCmp(hideCompId[i]).hide();
+		}
+
+		for ( var j = 0; j < showCompId.length; j++) {
 			Ext.getCmp(showCompId[j]).show();
 		}
 	},
-	
-	//오른쪽으로 이동하는 에니메이션
-	onMoveSlideRight : function(title, movePanelId, hideCompId, showCompId){
+
+	// 오른쪽으로 이동하는 에니메이션
+	onMoveSlideRight : function(title, movePanelId, hideCompId, showCompId) {
 		Ext.getCmp('titlebar').setTitle(title);
-		Ext.getCmp('mainPanel').animateActiveItem(Ext.getCmp(movePanelId), {
-			type : 'slide',
-			direction : 'right',
-		});
-		
-		for(var i=0; i<hideCompId.length; i++){
+
+		if (typeof (movePanelId) == 'string') {
+			Ext.getCmp('mainPanel').animateActiveItem(Ext.getCmp(movePanelId),
+					{
+						type : 'slide',
+						direction : 'right',
+					});
+		} else if (typeof (movePanelId) == 'number') {
+			Ext.getCmp('mainPanel').animateActiveItem(movePanelId, {
+				type : 'slide',
+				direction : 'right',
+			});
+		}
+
+		for ( var i = 0; i < hideCompId.length; i++) {
 			Ext.getCmp(hideCompId[i]).hide();
 		}
-		
-		for(var j=0; j<showCompId.length; j++){
-			Ext.getCmp(showComp[j]).show();
+
+		for ( var j = 0; j < showCompId.length; j++) {
+			Ext.getCmp(showCompId[j]).show();
 		}
 	}
 });
